e702138bb13fdc3a6d8b2deda8a4bb11b6897e18
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2024-04-07 20:00:29 +0530

    userControl Extra Features

--- Controller/user.controller.js
+++ Controller/user.controller.js
@@ -10,37 +10,59 @@ import { sendCookie } from "../utils/features.js";
 
 
 export const createNewUser = async(req,res)=>{
-    const {name, username, email, password} = req.body
+    try {
+        const {name, username, email, password} = req.body
+    
+        let user = await User.findOne({username});
+    
+        const avatarLocalPath = req.files?.avatar[0]?.path
 
-    let user = await User.findOne({username});
-
-    const avatarLocalPath = req.files?.avatar[0]?.path
-
-    const avatar = await uploadOnCloudinary(avatarLocalPath)
-
-    const hashedPassword = await bcrypt.hash(password, 10)
-
-    if(user){
-        return res.status(404).json({
-            success: false,
-            message: "User already exists",
+        if(!name){
+            console.log("Name not Found");
+        }
+        if(!username){
+            console.log("username not Found");
+        }
+        if(!email){
+            console.log("email not Found");
+        }
+        if(!password){
+            console.log("password not Found");
+        }
+        if(!avatarLocalPath){
+            console.log("avatarLocalPath not Found");
+        }
+    
+        
+    
+        const avatar = await uploadOnCloudinary(avatarLocalPath)
+    
+        const hashedPassword = await bcrypt.hash(password, 10)
+    
+        if(user){
+            return res.status(404).json({
+                success: false,
+                message: "User already exists",
+            })
+        }
+    
+        user = await User.create({
+            name,
+            username: username.toLowerCase(),
+            email,
+            password:hashedPassword,
+            avatar: avatar?.url || "",
+    
         })
+    
+        res.status(200).json({
+            success: true,
+            message: "Registered Succesfully",
+            imageUrl: avatar.url
+        })
+    } catch (error) {
+        throw new ApiError(400, "Problem in creating User", error)
     }
-
-    user = await User.create({
-        name,
-        username: username.toLowerCase(),
-        email,
-        password:hashedPassword,
-        avatar: avatar?.url || "",
-
-    })
-
-    res.status(200).json({
-        success: true,
-        message: "Registered Succesfully",
-        imageUrl: avatar.url
-    })
 }
 
 
