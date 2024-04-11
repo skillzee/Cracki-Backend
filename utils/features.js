848398b623548f1b4d6d5f572e07b5f43960f009
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2024-04-11 20:43:10 +0530

    kax

--- utils/features.js
+++ utils/features.js
@@ -8,7 +8,7 @@ export const sendCookie = (user, res, message, statusCode = 200)=>{
         httpOnly: true,
         maxAge: 15 * 60 * 1000,
         // sameSite: process.env.NODE_ENV == "DEVELOPMENT"?"lax": "none",
-        sameSite:  "none",
+        sameSite:  "lax",
         // secure:process.env.NODE_ENV == "DEVELOPMENT"?false: true
         secure:false,
         domain: "localhost",
