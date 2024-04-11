f658c96d91f8f44b7435ba325ef90e4046f9913b
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2024-04-11 23:05:39 +0530

    cookies

--- Controller/user.controller.js
+++ Controller/user.controller.js
@@ -99,8 +99,7 @@ export const logInUser = async(req, res,next)=>{
 
 export const logout = (req,res)=>{
     res.status(200).cookie("token", "", {expires: new Date(Date.now()),
-        sameSite: process.env.NODE_ENV == "DEVELOPMENT"?"lax": "none",
-        secure:process.env.NODE_ENV == "DEVELOPMENT"?false: true
+        httpOnly: true
     }).json({
         success: true,
         message: "Logged Out Successfully",
--- utils/features.js
+++ utils/features.js
@@ -5,14 +5,7 @@ export const sendCookie = (user, res, message, statusCode = 200)=>{
     const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET)
 
     res.status(statusCode).cookie("token", token,{
-        httpOnly: true,
-        maxAge: 15 * 60 * 1000,
-        // sameSite: process.env.NODE_ENV == "DEVELOPMENT"?"lax": "none",
-        sameSite:  "lax",
-        // secure:process.env.NODE_ENV == "DEVELOPMENT"?false: true
-        secure:false,
-        domain: "localhost",
-        path: "/"
+        httpOnly: true
     }).json({
         success: true,
         message: message,
