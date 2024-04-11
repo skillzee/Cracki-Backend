d55e63eef9906e72a81f49b3310042d2d20ca4ca
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2024-04-12 00:32:46 +0530

    cookie

--- utils/features.js
+++ utils/features.js
@@ -5,7 +5,9 @@ export const sendCookie = (user, res, message, statusCode = 200)=>{
     const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET)
 
     res.status(statusCode).cookie("token", token,{
-        httpOnly: true
+        httpOnly: true,
+        secure: false,
+        sameSite: "none",
     }).json({
         success: true,
         message: message,
