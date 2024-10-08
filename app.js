1afcc6c116db1cd917ab5b064cece33546c03cc3
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2024-10-08 16:53:51 +0530

    cors

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ config({
 app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
-    origin: "*",
+    origin: process.env.FRONTEND_URI,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }))
